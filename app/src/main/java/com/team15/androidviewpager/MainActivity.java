d91c1392ff578dc714d281262c99358999bcde55
sazzad <shfahad.920@gmail.com>
2020-09-19 00:53:26 +0600

    First commit

---
+++ app/src/main/java/com/team15/androidviewpager/MainActivity.java
@@ -0,0 +1,56 @@
+package com.team15.androidviewpager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.TextView;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+    //sazzad765
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private int[] tabIcons = {
+            R.drawable.ic_baseline_home_24,
+            R.drawable.ic_baseline_work_24,
+            R.drawable.ic_baseline_watch_later_24,
+    };
+    private String[] tabTitles = {
+            "Home", "Work", "Watch"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewPager = findViewById(R.id.pager);
+        setupViewPager(viewPager);
+
+        tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
+        setupTabIcons();
+    }
+
+    private void setupTabIcons() {
+        for (int i = 0; i < tabIcons.length; i++) {
+            TextView tabOne = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+            tabOne.setText(tabTitles[i]);
+            tabOne.setCompoundDrawablesWithIntrinsicBounds(0, tabIcons[i], 0, 0);
+            tabLayout.getTabAt(i).setCustomView(tabOne);
+        }
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager());
+        adapter.addFrag(new HomeFragment());
+        adapter.addFrag(new WorkFragment());
+        adapter.addFrag(new WatchLaterFragment());
+        viewPager.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/com/team15/androidviewpager/PagerAdapter.java
@@ -0,0 +1,32 @@
+package com.team15.androidviewpager;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PagerAdapter extends FragmentPagerAdapter {
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+
+    public PagerAdapter(FragmentManager manager) {
+        super(manager);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    public void addFrag(Fragment fragment) {
+        mFragmentList.add(fragment);
+    }
+
+
+}
